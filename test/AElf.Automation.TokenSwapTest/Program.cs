c89b2da7aafe52025dc6f3a8c0775f3ca4bf17b1
YueZh127 <jing3san@gmail.com>
2020-05-18 15:44:52 +0800

    change round

--- test/AElf.Automation.TokenSwapTest/Program.cs
+++ test/AElf.Automation.TokenSwapTest/Program.cs
@@ -33,7 +33,7 @@ namespace AElf.Automation.TokenSwapTest
                 if (TreeInfos.Count != 0)
                 {
                     var root = TreeInfos[index].MerkleRoot;
-                    if (TokenSwapRound != currentRound || currentRound ==0 )
+                    if (TokenSwapRound > currentRound -1 || currentRound ==0 )
                     {
                         Logger.Info($"Add {index} round: {root}");
                         await tokenSwap.AddSwapRound(root, index);
